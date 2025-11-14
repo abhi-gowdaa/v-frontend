0df87fca71f00bc6a0eed62f7f438a0aa1690794
Abhishek B S <109302022+abhi-gowdaa@users.noreply.github.com>
2025-11-14 08:40:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
   return (
     <div>
       <h2>Drag and Drop the Nodes(Input, llm, text ,output) rest are demo, submit to get if its dag or not, run pipline for the chat interface</h2>
+    <div>
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
       <RunPipelineDrawer />
+    </div>
     </div>
   );
 }
